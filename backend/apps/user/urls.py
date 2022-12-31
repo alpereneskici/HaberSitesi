91a14a9d675b5a11b58c079bcbfd3bf12a150205
Umut Bilgen <84351356+UmutBilgen@users.noreply.github.com>
2022-12-31 23:27:56 +0300

    fix: url name change

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from .views import UserLoginView,UserRegisterView,UserDetailsView,ForgetPasswordView,ResetForgetPasswordView
+from .views import UserLoginView,UserRegisterView,UserDetailsView,ForgetPasswordView,ResetForgetPasswordView,logout
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(),name='login'),
@@ -9,5 +9,5 @@ urlpatterns = [
     path('details/', UserDetailsView.as_view(),name='details'),
     path('forget-password/', ForgetPasswordView.as_view(),name='forget_passpord'),
     path('reset/', ResetForgetPasswordView.as_view(),name='reset'),
-    path('delete/', ResetForgetPasswordView.as_view(),name='delete'),
+    path('logout/', logout.as_view(),name='logout'),
 ]
